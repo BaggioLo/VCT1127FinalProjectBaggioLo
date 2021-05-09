2eb37953155438b007f4fa7bbcee54a55dc3c3c7
BaggioLo <72361665+BaggioLo@users.noreply.github.com>
2021-05-09 17:01:58 -0500

    added in jquery and gsap timeline

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 /* Effects for the banner page, the html will be coming in from
 the left, but the spaceship will be coming in from the top right
 I wanted to make it look like it was flying in from the
-top right, so I set a delay until after the text popped up
+top right, so I set a delay until after the text popped up. 
+Incorporating gsap timeline to make sure that they come one after the other
 */
-gsap.from('.effect1', {opacity: 0, duration: 2, x: -50, stagger: 1});
-gsap.from('.effect2', {opacity: 0, duration: 1, x:50, y: -50, delay: 2});
+
+var tl = gsap.timeline({defaults:{duration: 1}})
+
+tl.from(".effect1", {x: -50, stagger: 1, opacity: 0, duration: 2})
+    .from(".effect2", {opacity: 0, x: 50, y: -50, duration: 2},  "-=1.2");
+
+
