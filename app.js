92be7a4deaaceef8a3712c710566c61e5311b655
BaggioLo <72361665+BaggioLo@users.noreply.github.com>
2021-05-09 20:24:31 -0500

    Added Scroll Trigger

--- app.js
+++ app.js
@@ -5,9 +5,28 @@ top right, so I set a delay until after the text popped up.
 Incorporating gsap timeline to make sure that they come one after the other
 */
 
-var tl = gsap.timeline({defaults:{duration: 1}})
+let tl = gsap.timeline({defaults:{duration: 1}})
 
 tl.from(".effect1", {x: -50, stagger: 1, opacity: 0, duration: 2})
     .from(".effect2", {opacity: 0, x: 50, y: -50, duration: 2},  "-=1.2");
 
+/* Scroll Trigger section 
+I wanted to create a little scroll as the user
+is moving onto the features section that the spaceship
+will shoot across the screen
+*/
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".spaceshipscroll", {
+    scrollTrigger: {
+    trigger: ".spaceshipscroll",
+    markers: true,
+    start: "top center",
+    end: "top 100px",
+    scrub: true
+    },
+    x: 1700,
+    duration: 8
+});
 
